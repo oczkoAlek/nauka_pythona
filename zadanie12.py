c0c33e1630450a1dca1bc79da83a90164d7a8789
oczkoAlek <oczkoAlek@users.noreply.github.com>
2020-12-05 15:45:33 +0100

    komit zadania 12

--- zadanie12.py
+++ zadanie12.py
@@ -6,12 +6,24 @@ suma = 0
 for pozycja in koszyk:
     print('{0}: {1}'.format(pozycja['nazwa'], pozycja['cena']))
     suma = suma + pozycja['cena']
+
 print('Suma: ' + str(suma))
 print(len(koszyk))
 
 if len(koszyk) > 3 and suma < 500:
     suma_po_rabacie = suma - ((suma * 5)/100)
     print('Suma po rabacie 5%: ' + str(suma_po_rabacie))
+
 if suma > 500:
     suma_po_rabacie = suma - ((suma * 10)/100)
     print('Suma po rabacie 10%: ' + str(suma_po_rabacie))
+
+else:
+    suma_po_rabacie = suma
+
+def najtanszy(e):
+    return e['cena']
+#print(suma)
+koszyk.sort(key=najtanszy)
+suma_po_rabacie = suma_po_rabacie - koszyk[0]['cena']
+print('Suma po odjeciu najtanszego: ' + str(suma_po_rabacie))
